7270210139660c1ee5eca128dd2edd5ee8d645ad
Alex Birch <alexander.birch@billforward.net>
2015-08-10 18:31:16 +0100

    attempt to consult managed managed account

--- config/config.example.js
+++ config/config.example.js
@@ -1,3 +1,4 @@
 module.exports = {
 	stripeAPIKey: 'your stripe API key',
+	stripeManagedAccount: 'managed stripe account to consult'
 };
--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 var config = require('./config/config')
 var stripe = require('stripe')(config.stripeAPIKey);
+
+stripe.accounts.listExternalAccounts(
+	config.stripeManagedAccount,
+	{
+		object: "card"
+	})
+	.then(console.log);
