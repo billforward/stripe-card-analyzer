53d1925bd29a557ece11a19caf8e30e542dc3ff8
Alex Birch <alexander.birch@billforward.net>
2015-08-10 18:53:04 +0100

    input

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-var config = require('./config/config')
+var config = require('./config/config');
 var stripe = require('stripe')(config.stripeAPIKey);
+stripe.setApiVersion('2015-07-13');
 
-stripe.accounts.listExternalAccounts(
-	config.stripeManagedAccount,
-	{
-		object: "card"
-	})
-	.then(console.log);
+// stripe.accounts.listExternalAccounts(
+// 	config.stripeManagedAccount,
+// 	{
+// 		object: "card"
+// 	})
+// 	.then(console.log);
+
+// stripe.accounts.list()
+// .then(console.log);
+
+stripe.customers.list()
+.then(console.log);
+
+// stripe.accounts.retrieve(
+//   config.stripeManagedAccount)
+// .then(console.log);
